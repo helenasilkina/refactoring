7caefa359e34f6717c7196d25d7fb86b35147850
Helena Silkina <helenasilkina@gmail.com>
2015-07-28 21:32:38 +0300

    Update Inline Method.py

--- Inline Method.py
+++ Inline Method.py
@@ -1,16 +1,12 @@
-// After
-
+# After
 class PizzaDelivery:
-    #...
     def getRating(self):
         return 2 if self.moreThanFiveLateDeliveries() else 1
   
     def moreThanFiveLateDeliveries(self):
         return self.numberOfLateDeliveries > 5
 
-//Refactoring
-
+# Refactoring
 class PizzaDelivery:
-  #...
   def getRating(self):
     return 2 if self.numberOfLateDeliveries > 5 else 1
