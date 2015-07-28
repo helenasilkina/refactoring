259aa5afaa65cee06e7f8c046fdb543f61df59ce
Helena Silkina <helenasilkina@gmail.com>
2015-07-28 21:25:21 +0300

    Create Split_Temporary_Variable.py

---
+++ Split_Temporary_Variable.py
@@ -0,0 +1,13 @@
+// After
+
+temp = 2 * (height + width)
+print(temp)
+temp = height * width
+print(temp)
+
+// Refactoring
+
+perimeter = 2 * (height + width)
+print(perimeter);
+area = height * width
+print(area)
