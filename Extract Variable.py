4cf6817bc3ec12d9605ed273d7928ceec2430b6a
Helena Silkina <helenasilkina@gmail.com>
2015-07-28 21:37:03 +0300

    Create Extract Variable.py

---
+++ Extract Variable.py
@@ -0,0 +1,15 @@
+# After
+def renderBanner(self):
+    if (self.platform.toUpperCase().indexOf("MAC") > -1) and \
+       (self.browser.toUpperCase().indexOf("IE") > -1) and \
+       self.wasInitialized() and (self.resize > 0):
+        # do something
+        
+# Refactoring
+def renderBanner(self):
+    isMacOs = self.platform.toUpperCase().indexOf("MAC") > -1
+    isIE = self.browser.toUpperCase().indexOf("IE") > -1
+    wasResized = self.resize > 0
+
+    if isMacOs and isIE and self.wasInitialized() and wasResized:
+        # do something
