2de86f65c14f1fa53f6d9c3bcedca70b1dcd301c
Helena Silkina <helenasilkina@gmail.com>
2015-07-29 12:23:33 +0300

    Create Replace Temp with Query.py

---
+++ Replace Temp with Query.py
@@ -0,0 +1,17 @@
+# After
+def calculateTotal():
+    basePrice = quantity * itemPrice
+    if basePrice > 1000:
+        return basePrice * 0.95
+    else:
+        return basePrice * 0.98
+
+# Refactoring
+def calculateTotal():
+    if basePrice() > 1000:
+        return basePrice() * 0.95
+    else:
+        return basePrice() * 0.98
+
+def basePrice():
+    return quantity * itemPrice
