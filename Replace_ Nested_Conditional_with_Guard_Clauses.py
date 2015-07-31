df7e1f252a8a2f25489b1e0ced8219e44a88cf2a
Helena Silkina <helenasilkina@gmail.com>
2015-07-31 11:15:09 +0300

    Create Replace_ Nested_Conditional_with_Guard_Clauses.py

---
+++ Replace_ Nested_Conditional_with_Guard_Clauses.py
@@ -0,0 +1,23 @@
+# After
+def getPayAmount(self):
+    if self.isDead:
+        result = deadAmount()
+    else:
+        if self.isSeparated:
+            result = separatedAmount()
+        else:
+            if self.isRetired:
+                result = retiredAmount()
+            else:
+                result = normalPayAmount()
+    return result
+
+# Refactoring
+def getPayAmount(self):
+    if self.isDead:
+        return deadAmount()
+    if self.isSeparated:
+        return separatedAmount()
+    if self.isRetired:
+        return retiredAmount()
+    return normalPayAmount()
