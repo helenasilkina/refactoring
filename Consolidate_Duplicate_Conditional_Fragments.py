595db34bad533d5c1504f09d305b9a75b625f3de
Helena Silkina <helenasilkina@gmail.com>
2015-07-31 11:08:57 +0300

    Create Consolidate_Duplicate_Conditional_Fragments.py

---
+++ Consolidate_Duplicate_Conditional_Fragments.py
@@ -0,0 +1,14 @@
+# After
+if isSpecialDeal():
+    total = price * 0.95
+    send()
+else:
+    total = price * 0.98
+    send()
+
+# Refactoring
+if isSpecialDeal():
+    total = price * 0.95
+else:
+    total = price * 0.98
+send()
