665e326a1ba8d8a2e1edae079058dccb30288d2b
Helena Silkina <helenasilkina@gmail.com>
2015-08-01 10:37:17 +0300

    Create Introduce_Null_Object.py

---
+++ Introduce_Null_Object.py
@@ -0,0 +1,16 @@
+# After
+if customer == None:
+    plan = BillingPlan.basic()
+else:
+    plan = customer.getPlan()
+
+# Refactoring
+class NullCustomer(Customer):
+    def isNull(self):
+        return True
+    def getPlan(self):
+        return self.NullPlan()
+# Replace null values with Null-object.
+customer = order.customer if order.customer != None else NullCustomer()
+# Use Null-object as if it's normal subclass.
+plan = customer.getPlan()
