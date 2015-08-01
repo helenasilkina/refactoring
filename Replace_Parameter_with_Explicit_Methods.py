f71f72a669c72ec2af150639326b3753b8c5781e
Helena Silkina <helenasilkina@gmail.com>
2015-08-01 15:30:22 +0300

    Create Replace_Parameter_with_Explicit_Methods.py

---
+++ Replace_Parameter_with_Explicit_Methods.py
@@ -0,0 +1,13 @@
+# After
+def output(self, type):
+    if name == "banner"
+        # Print the banner.
+        
+    if name == "info"
+
+# Refactoring
+def outputBanner(self):
+    # Print the banner.
+    
+def outputInfo(self):
+    # Print the info.
