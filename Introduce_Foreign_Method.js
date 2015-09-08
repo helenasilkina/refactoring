8386e0de5411a5fc2567baa1030292c8cbb2b336
Helena Silkina <helenasilkina@gmail.com>
2015-09-08 10:59:41 +0300

    Create Introduce_Foreign_Method.js

---
+++ Introduce_Foreign_Method.js
@@ -0,0 +1,17 @@
+// After
+class Report {
+  function sendReport(self) {
+    var nextDay = Date(self.previousEnd.getYear(), self.previousEnd.getMonth(), self.previousEnd.getDate() + 1);
+  }        
+}            
+
+// Refactoring
+class Report {
+  function sendReport(self) {
+    var newStart = self._nextDay(self.previousEnd);
+  }      
+        
+  function _nextDay(self, arg) {
+    return Date(arg.getYear(), arg.getMonth(), arg.getDate() + 1);
+  }  
+}        
