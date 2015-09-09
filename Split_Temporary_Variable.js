ffbdcd9fd2ab54d65c64d7fb81e3c906377d9903
Helena Silkina <helenasilkina@gmail.com>
2015-09-09 10:59:08 +0300

    Create Split_Temporary_Variable.js

---
+++ Split_Temporary_Variable.js
@@ -0,0 +1,11 @@
+// After
+var temp = 2 * (height + width);
+console.log(temp);
+temp = height * width;
+console.log(temp);
+
+// Refactoring
+var perimeter = 2 * (height + width);
+console.log(perimeter);
+var area = height * width;
+console.log(area);
