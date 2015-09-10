f168bdf654d8bd12ecadcb25a26350fdcb05f835
Helena Silkina <helenasilkina@gmail.com>
2015-09-10 11:32:17 +0300

    Create Replace_Method_with_Method_Object.js

---
+++ Replace_Method_with_Method_Object.js
@@ -0,0 +1,27 @@
+// After
+class Order() {
+  function price(self) {
+    var primaryBasePrice = 0;
+    var secondaryBasePrice = 0;
+    var tertiaryBasePrice = 0;
+  }  
+}
+
+// Refactoring
+class Order() {
+  function price(self) {
+    return PriceCalculator(self).compute();
+  }      
+}        
+
+class PriceCalculator() {
+  self._primaryBasePrice = 0
+  self._secondaryBasePrice = 0
+  self._tertiaryBasePrice = 0
+
+  // copy relevant information from order object.
+  function __init__(self, order) {}    
+
+  # long computation.
+  function compute(self) {}
+}        
