7f95032f470a5ff419e24f7b6dc0ddea11462554
Helena Silkina <helenasilkina@gmail.com>
2015-09-10 11:39:40 +0300

    Create Replace_Parameter_with_Explicit_Methods.js

---
+++ Replace_Parameter_with_Explicit_Methods.js
@@ -0,0 +1,18 @@
+// After
+function output(self, type) {
+  if (name == "banner") {
+    // Print the banner.
+  }    
+      
+  if (name == "info") {
+  
+  }
+}    
+
+// Refactoring
+
+// Print the banner.
+function outputBanner(self) {}    
+
+// Print the info.    
+function outputInfo(self) {}
