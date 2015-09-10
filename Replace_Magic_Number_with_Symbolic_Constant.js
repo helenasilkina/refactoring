8ad120e5b463222cf0dd1f6d401a955a1aaad024
Helena Silkina <helenasilkina@gmail.com>
2015-09-10 11:28:53 +0300

    Create Replace_Magic_Number_with_Symbolic_Constant.js

---
+++ Replace_Magic_Number_with_Symbolic_Constant.js
@@ -0,0 +1,11 @@
+// After
+function potentialEnergy(mass, height) {
+  return mass * height * 9.81;
+}  
+
+// Refactoring
+var GRAVITATIONAL_CONSTANT = 9.81
+
+function potentialEnergy(mass, height) {
+  return mass * height * GRAVITATIONAL_CONSTANT;
+}  
