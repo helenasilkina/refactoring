524afc3528dfa11927e0904941c8bbf60dc1ffa3
Helena Silkina <helenasilkina@gmail.com>
2015-10-05 11:37:03 +0300

    change variable name

--- Introduce_Foreign_Method.py
+++ Introduce_Foreign_Method.py
@@ -7,7 +7,7 @@ class Report:
 # Refactoring
 class Report:
     def sendReport(self):
-        newStart = self._nextDay(self.previousEnd)
+        nextDay = self._nextDay(self.previousEnd)
 
     def _nextDay(self, arg):
         return Date(arg.getYear(), arg.getMonth(), arg.getDate() + 1)
